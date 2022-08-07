9a09189b5f9967fe58bd49ae091085d81da7eb2f
Dharmendra <dharmendra.patel264@gmail.com>
2022-08-07 14:08:34 +0530

    Added server file

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
 //import express for routing
 const express = require("express");
 const cors = require('cors');
-const morgan = require("morgan");
-const logger = require("./src/logger/logger");
-const swaggerUi = require('swagger-ui-express');
 
 //import body-parser
 const bodyParser = require("body-parser");
-//import http
-const http = require("http");
-//import dotenv
-require('dotenv').config()
 
-const swaggerDocument = require('./config/swagger.json');
 const app = express();
 
 app.use(cors());
-
-// Swagger Documentation
-app.server = http.createServer(app);
-app.use('/api/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
@@ -27,30 +15,26 @@ app.use(express.urlencoded({ limit: '50mb' }));
 var jsonParser = bodyParser.json()
 app.use(jsonParser);
 
-const ApiRoutes = require("./src/routes/index");
-app.use('/api', ApiRoutes);
+// const ApiRoutes = require("./src/routes/index");
+// app.use('/api', ApiRoutes);
 
-// HTTP request logger middleware for node.js
-app.use(morgan("dev"));
+// app.use((error, req, res, next) => {
+//             logger.error(`${404} - ${`No route found`} - ${req.originalUrl} - ${req.method} - ${req.ip}`
+//   );
+//   res.status(error.status || 500);
+//   res.json({
+//     error: {
+//       message: error.message,
+//     },
+//   });
+// });
 
-app.use((error, req, res, next) => {
-            logger.error(`${404} - ${`No route found`} - ${req.originalUrl} - ${req.method} - ${req.ip}`
-  );
-  res.status(error.status || 500);
-  res.json({
-    error: {
-      message: error.message,
-    },
-  });
+// app.use((req, res) => {
+//     res.status(404).json({
+//       message: `Route ${req.url} Not found.`
+//     });
+//   });
+
+app.listen(5000, (req, res) => {
+    console.log(`The server is running on port : 5000`);
 });
-
-app.use((req, res) => {
-    res.status(404).json({
-      message: `Route ${req.url} Not found.`
-    });
-  });
-
-app.listen(process.env.PORT,(req,res)=>{
-  logger.info(`${200} - ${`server running on port`} ${process.env.PORT}`);
-  console.log(`The server is running on port :`,process.env.PORT);
-})
